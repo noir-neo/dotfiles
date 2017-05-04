1bd9c491640b0a6d91b7d31197181486ab23666d
Shoma SATO <noir.neo.04@gmail.com>
2017-05-04 22:29:03 +0900

    Add tigrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.tigrc ~/.tigrc
