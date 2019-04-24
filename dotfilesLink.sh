dc44b8553009f6f13f3acf8b0067a8c5e872ca41
Shoma SATO <noir.neo.04@gmail.com>
2019-04-24 15:28:51 +0900

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,7 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
+
+
