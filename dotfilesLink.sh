4e77b02f3ac6709e79ef7c42022977fa357c36f8
Shoma SATO <noir.neo.04@gmail.com>
2021-01-30 23:13:13 +0900

    Update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.tigrc ~/.tigrc
-ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
+find . -type f -name .\* -print0 | xargs -0 -I{} ln -sf `pwd`/{} ~/{}
 
